80e1ba838d159bd8ec0376879064ce3fe83dfbaf
Taylor Freiner <o2-freiner@hoare7.cs.umsl.edu>
2017-09-09 13:01:03 -0500

    Working on #8

--- program3_1_8.c
+++ program3_1_8.c
@@ -1,8 +1,8 @@
 /*
  * Program 3.1 - Number 8 
  * Author: Taylor Freiner
- * Date: 9-7-17
- * Log: Building string from characters entered into stdin
+ * Date: 9-9-17
+ * Log: Reworking loop logic and passing nchars variable at command line 
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,26 +10,28 @@
 
 int main (int argc, char *argv[]) {
 	pid_t childpid = 0;
-	int i, n;
-	if (argc != 2){   /* check for valid number of command-line arguments */
+	int i, j, n, nchars;
+	char x;
+	if (argc != 3){   /* check for valid number of command-line arguments */
 		fprintf(stderr, "Usage: %s processes\n", argv[0]);
 		return 1;
 	}
 	n = atoi(argv[1]);
-	for (i = 1; i < n; i++){
+	nchars = atoi(argv[2]);
+	char mybuf[nchars+1];
+
+	for (i = 1; i < n; i++) {
 		if (childpid = fork())
 			break;
 	}
-	
-	int c;
-	char buff[1000];
 
-	c = fgetc(stdin);
-	while(!feof(stdin)){
-		ungetc(c, stdin);
-		fgets(buff, 1000, stdin);
-		c = fgetc(stdin);
+	//TODO figure out why this is not waiting for nchars characters after the first process	
+	for (j = 0; j < nchars; j++) {
+		mybuf[j] = getc(stdin);
 	}
+	mybuf[nchars] = '\0';
 
-	return 0;
+	fprintf(stderr, "i:%d  process ID:%ld  mybuf:%s\n",
+                        i, (long)getpid(), mybuf);
+        return 0;
 }
